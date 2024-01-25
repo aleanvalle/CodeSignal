abb93fcc4bb318c6c6c854452dd5d94b0d2d59e6
aleanvalle <alexv4lle@gmail.com>
2024-01-25 09:04:51 -0300

    agregado AllLongestStrings

---
+++ CSharp/Arcade/Intro/SmoothSailing/AllLongestStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AllLongestStrings
+{
+    internal class Program
+    {
+        string[] AllLongestStrings(string[] inputArray)
+        {
+            int maxLength = inputArray.Select(x => x.Length).ToArray().Max();
+            return inputArray.Where(x => x.Length == maxLength).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            string[] b = ["aba", "aa", "ad", "vcd", "aba"];
+            Console.WriteLine("result: " + string.Join(", ", a.AllLongestStrings(b)));
+        }
+    }
+}
