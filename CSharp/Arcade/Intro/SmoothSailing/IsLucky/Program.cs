c78769f36d26418abdaee269fde6c6402cf921a8
aleanvalle <alexv4lle@gmail.com>
2024-01-25 11:21:25 -0300

    agregado IsLucky

---
+++ CSharp/Arcade/Intro/SmoothSailing/IsLucky/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IsLucky
+{
+    internal class Program
+    {
+        bool IsLucky(int n)
+        {
+            int limit = n.ToString().Length;
+            int half = limit / 2;
+            var firstPart = n.ToString().ToList().Slice(0, half);
+            var secondPart = n.ToString().ToList().Slice(half, limit - half);
+            var firstNumPart = firstPart.Select(x => int.Parse(x.ToString()));
+            var secondNumPart = secondPart.Select(x => int.Parse(x.ToString()));
+            return firstNumPart.Sum() == secondNumPart.Sum();
+        } 
+
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            int n1 = 1230;
+            int n2 = 239017;
+            Console.WriteLine("result: " + a.IsLucky(n1));
+            Console.WriteLine("result: " + a.IsLucky(n2));
+        }
+    }
+}
