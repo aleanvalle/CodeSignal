714a8263283a06c086030dc58544786a1a1025fd
aleanvalle <alexv4lle@gmail.com>
2024-01-25 12:27:40 -0300

    agregado SortByHeight

---
+++ CSharp/Arcade/Intro/SmoothSailing/SortByHeight/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SortByHeight
+{
+    internal class Program
+    {
+        int[] SortByHeight(int[] a)
+        {
+            Dictionary<int, int> tressPos = new Dictionary<int, int>();
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] == -1)
+                {
+                    tressPos[i] = a[i];
+                }
+            }
+            var orderedHeights = a.Order().ToList();
+            orderedHeights.RemoveAll(x => x == -1);
+            for (int t = 0; t < tressPos.Count; t++)
+            {
+                orderedHeights.Insert(tressPos[t], tressPos[t]);
+            }
+            return orderedHeights.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            int[] heights1 = [-1, 150, 190, 170, -1, -1, 160, 180];
+            Console.WriteLine("result: " + a.SortByHeight(heights1));
+        }
+    }
+}
