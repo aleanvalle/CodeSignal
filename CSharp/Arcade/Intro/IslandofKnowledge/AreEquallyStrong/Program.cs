420a070635e95175fffa8ad6a73055993b480e11
aleanvalle <alexv4lle@gmail.com>
2024-01-30 10:49:56 -0300

    agregado AreEquallyStrong

---
+++ CSharp/Arcade/Intro/IslandofKnowledge/AreEquallyStrong/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AreEquallyStrong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
