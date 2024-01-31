278263cb4fa22e3ddc4bb8f9229c2369c45ca93b
aleanvalle <alexv4lle@gmail.com>
2024-01-31 11:48:13 -0300

    agregado IsIPv4Address

---
+++ CSharp/Arcade/Intro/IslandofKnowledge/IsIPv4Address/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IsIPv4Address
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
