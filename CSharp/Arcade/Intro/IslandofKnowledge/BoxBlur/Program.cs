0ba1c319246c27dd5dba760aa9fb868b5dd2cd10
aleanvalle <alexv4lle@gmail.com>
2024-02-05 12:09:23 -0300

    arreglada logica

--- CSharp/Arcade/Intro/IslandofKnowledge/BoxBlur/Program.cs
+++ CSharp/Arcade/Intro/IslandofKnowledge/BoxBlur/Program.cs
@@ -2,35 +2,54 @@
 {
     internal class Program
     {
-        int printMatrix(int[][] arr)
+        string printMatrix(int[][] arr)
         {
             int rowLength = arr.Length;
             int colLength = arr[0].Length;
-
+            Console.Write("[");
             for (int i = 0; i < rowLength; i++)
             {
+                Console.Write("[");
                 for (int j = 0; j < colLength; j++)
                 {
-                    Console.Write(string.Format("{0} ", arr[j]));
+                    Console.Write(string.Format("{0}", arr[i][j]));
                 }
-                Console.Write(string.Format("{0} ", arr[i]));
+                Console.Write("], \n");
             }
-            Console.ReadLine();
-            return 0;
+            Console.Write("]");
+            return "end";
+        }
+
+        int CorneredBlur(int[][] image, (int,int) leftUpperCornerGenerator)
+        {
+            double sum = 0;
+            sum += image[leftUpperCornerGenerator.Item1][leftUpperCornerGenerator.Item2] + image[leftUpperCornerGenerator.Item1][leftUpperCornerGenerator.Item2 + 1] + image[leftUpperCornerGenerator.Item1][leftUpperCornerGenerator.Item2 + 2]
+                   + image[leftUpperCornerGenerator.Item1 + 1][leftUpperCornerGenerator.Item2] + image[leftUpperCornerGenerator.Item1 + 1][leftUpperCornerGenerator.Item2 + 1] + image[leftUpperCornerGenerator.Item1 + 1][leftUpperCornerGenerator.Item2 + 2]
+                   + image[leftUpperCornerGenerator.Item1 + 2][leftUpperCornerGenerator.Item2] + image[leftUpperCornerGenerator.Item1 + 2][leftUpperCornerGenerator.Item2 + 1] + image[leftUpperCornerGenerator.Item1 + 2][leftUpperCornerGenerator.Item2 + 2];
+            double result = Math.Floor(sum / 9);
+            return Convert.ToInt32(result);
         }
 
         int[][] BoxBlur(int[][] image)
         {
-            double sum = 0;
-            for (int i = 0; i < image.Length; i++)
+            int ZERO = 0;
+            if (image.Length == 3 && image[0].Length == 3)
             {
-                for(int j = 0; j < image[i].Length; j++)
-                {
-                    sum += image[i][j];
-                }
+                return new int[][] { new int[] { CorneredBlur(image, (ZERO, ZERO)) } };
             }
-            double result = Math.Floor(sum / (image.Length * image[0].Length));
-            return new int[][] { new int[] { Convert.ToInt32(result) } };
+            else
+            {
+                int xLimit = image[0].Length - 1;
+                int yLimit = image.Length - 1;
+                int leftUpper = CorneredBlur(image, (ZERO, ZERO));
+                int rightUpper = CorneredBlur(image, (ZERO, yLimit - 2));
+                int leftLower = CorneredBlur(image, (xLimit - 2, ZERO));
+                int rightLower = CorneredBlur(image, (xLimit - 2, yLimit - 2));
+                return new int[][] { 
+                    new int[] { leftUpper, rightUpper },
+                    new int[] { leftLower, rightLower },
+                };
+            }
         }
 
         static void Main(string[] args)
@@ -47,8 +66,8 @@
                 new int[] { 6, 10, 7, 8},
                 new int[] { 1, 4, 2, 0}
             };
-            Console.WriteLine("b: " + a.printMatrix(a.BoxBlur(b)));
-            Console.WriteLine("c: " + a.printMatrix(a.BoxBlur(c)));
+            Console.WriteLine(" b: " + a.printMatrix(a.BoxBlur(b)));
+            Console.WriteLine(" c: " + a.printMatrix(a.BoxBlur(c)));
         }
     }
 }
