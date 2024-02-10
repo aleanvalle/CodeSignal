2c2ea137ef5e331d021dc0ff6d0ff32195e5d0ea
aleanvalle <alexv4lle@gmail.com>
2024-02-10 12:09:56 -0300

    arreglada logica

--- CSharp/Arcade/Intro/IslandofKnowledge/Minesweeper/Program.cs
+++ CSharp/Arcade/Intro/IslandofKnowledge/Minesweeper/Program.cs
@@ -1,90 +1,127 @@
-﻿using System.Drawing;
-
-namespace Minesweeper
+﻿namespace Minesweeper
 {
     internal class Program
     {
-        enum Position : int
+        struct MinesLocator((int, int) coordinate)
         {
-            LeftUpperCorner = 0,
-            RightUpperCorner = 1,
-            LeftLowerCorner = 2,
-            RightLowerCorner = 3,
-            Ceiling = 4,
-            Floor = 5,
-            LeftWall = 6,
-            RightWall = 7,
-            Free = 8
+            public List<(int, int)> LeftUpperCorner = new List<(int, int)>(){
+                (coordinate.Item1 + 1, coordinate.Item2),
+                (coordinate.Item1 + 1, coordinate.Item1 + 1),
+                (coordinate.Item1, coordinate.Item2 + 1)};
+            public List<(int, int)> RightUpperCorner = new List<(int, int)>(){
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 - 1),
+                (coordinate.Item1, coordinate.Item2 + 1)};
+            public List<(int, int)> LeftLowerCorner = new List<(int, int)>(){
+                (coordinate.Item1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2)};
+            public List<(int, int)> RightLowerCorner = new List<(int, int)>(){
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 - 1),
+                (coordinate.Item1, coordinate.Item2 - 1)};
+            public List<(int, int)> Ceiling = new List<(int, int)>(){
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 + 1),
+                (coordinate.Item1, coordinate.Item2 + 1),
+                (coordinate.Item1 + 1, coordinate.Item2 + 1),
+                (coordinate.Item1 + 1, coordinate.Item2)};
+            public List<(int, int)> Floor = new List<(int, int)>(){
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 - 1),
+                (coordinate.Item1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2)};
+            public List<(int, int)> LeftWall = new List<(int, int)>(){
+                (coordinate.Item1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2),
+                (coordinate.Item1 + 1, coordinate.Item2 + 1),
+                (coordinate.Item1, coordinate.Item2 + 1)};
+            public List<(int, int)> RightWall = new List<(int, int)>(){
+                (coordinate.Item1, coordinate.Item2 - 1),
+                (coordinate.Item1 - 1, coordinate.Item2 - 1),
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 + 1),
+                (coordinate.Item1, coordinate.Item2 + 1)};
+            public List<(int, int)> Free = new List<(int, int)>(){
+                (coordinate.Item1 - 1, coordinate.Item2 - 1),
+                (coordinate.Item1, coordinate.Item2 - 1),
+                (coordinate.Item1 + 1, coordinate.Item2 - 1),
+                (coordinate.Item1 - 1, coordinate.Item2),
+                (coordinate.Item1 + 1, coordinate.Item2),
+                (coordinate.Item1 - 1, coordinate.Item2 + 1),
+                (coordinate.Item1, coordinate.Item2 + 1),
+                (coordinate.Item1 + 1, coordinate.Item2 + 1)};
+        }
+        
+        List<bool> AddMines(bool[][] matrix, List<(int,int)> arrangement)
+        {
+            List<bool> positions = new List<bool>();
+            foreach ((int, int) adjacentPos in arrangement)
+            {
+                positions.Add(matrix[adjacentPos.Item1][adjacentPos.Item2]);
+            }
+            return positions;
         }
 
-        Position DefinePosition(int rowLimit, int colLimit, (int,int) coordinate)
+        int MineCounter(bool[][] matrix, (int,int) coordinate)
         {
-            if(coordinate.Item1 == 0 && coordinate.Item2 == 0)
+            List<bool> adjacentPositions;
+            int rowLimit = matrix.Length - 1;
+            int colLimit = matrix[0].Length; 
+            MinesLocator minesLocator = new MinesLocator(coordinate);
+            if(coordinate.Item1 == 0 && coordinate.Item2 == 0) //LeftUpperCorner
             {
-                return Position.LeftUpperCorner; //LeftUpperCorner
+                adjacentPositions = AddMines(matrix, minesLocator.LeftUpperCorner);    
             }
-            else if(coordinate.Item1 == 0 && coordinate.Item2 == colLimit)
+            else if(coordinate.Item1 == 0 && coordinate.Item2 == colLimit) //RightUpperCorner
             {
-                return Position.RightUpperCorner; //RightUpperCorner
+                adjacentPositions = AddMines(matrix, minesLocator.RightUpperCorner);
             }
-            else if(coordinate.Item1 == rowLimit && coordinate.Item2 == 0)
+            else if(coordinate.Item1 == rowLimit && coordinate.Item2 == 0) //LeftLowerCorner
             {
-                return Position.LeftLowerCorner; //LeftLowerCorner
+                adjacentPositions = AddMines(matrix, minesLocator.LeftLowerCorner);
             }
-            else if(coordinate.Item1 == rowLimit && coordinate.Item2 == colLimit)
+            else if(coordinate.Item1 == rowLimit && coordinate.Item2 == colLimit) //RightLowerCorner
             {
-                return Position.RightLowerCorner; //RightLowerCorner
+                adjacentPositions = AddMines(matrix, minesLocator.RightLowerCorner);
             }
-            else if(coordinate.Item1 == 0 && coordinate.Item2 < colLimit)
+            else if(coordinate.Item1 == 0 && coordinate.Item2 < colLimit) //Ceiling
             {
-                return Position.Ceiling; //Ceiling
+                adjacentPositions = AddMines(matrix, minesLocator.Ceiling);
             }
-            else if(coordinate.Item1 == rowLimit && coordinate.Item2 < colLimit)
+            else if(coordinate.Item1 == rowLimit && coordinate.Item2 < colLimit) //Floor
             {
-                return Position.Floor; //Floor
+                adjacentPositions = AddMines(matrix, minesLocator.Floor);
             }
-            else if(coordinate.Item1 < rowLimit && coordinate.Item2 == 0)
+            else if(coordinate.Item1 < rowLimit && coordinate.Item2 == 0) //LeftWall
             {
-                return Position.LeftWall; //LeftWall
+                adjacentPositions = AddMines(matrix, minesLocator.LeftWall);
             }
-            else if(coordinate.Item1 < rowLimit && coordinate.Item2 == colLimit)
+            else if(coordinate.Item1 < rowLimit && coordinate.Item2 == colLimit) //RightWall
             {
-                return Position.RightWall; //RightWall
+                adjacentPositions = AddMines(matrix, minesLocator.RightWall);
             }
-            else
+            else //Free
             {
-                return Position.Free; //Free
+                adjacentPositions = AddMines(matrix, minesLocator.Free);
             }
-        }
-
-        int MineCounter(bool[][] matrix, Position option, (int,int) coordinate)
-        {
-            List<bool> mineCounts = new List<bool>();
-            if(option == Position.LeftUpperCorner)
-            {
-                mineCounts.Add(matrix[coordinate.Item1 + 1][coordinate.Item2]);
-                mineCounts.Add(matrix[coordinate.Item1 + 1][coordinate.Item2 + 1]);
-                mineCounts.Add(matrix[coordinate.Item1][coordinate.Item2 + 1]);
-            }
-            else if(option == Position.RightUpperCorner)
-            {
-                mineCounts.Add(matrix[coordinate.Item1 - 1][coordinate.Item2]);
-                mineCounts.Add(matrix[coordinate.Item1 - 1][coordinate.Item2 - 1]);
-                mineCounts.Add(matrix[coordinate.Item1][coordinate.Item2 - 1]);
-            }
-            else if(option == Position.LeftLowerCorner)
-            {
-                mineCounts.Add(matrix[coordinate.Item1 + 1][coordinate.Item2]);
-                mineCounts.Add(matrix[coordinate.Item1 + 1][coordinate.Item2 - 1]);
-                mineCounts.Add(matrix[coordinate.Item1][coordinate.Item2 - 1]);
-            }
-            //all other options here...
-            return mineCounts.Count(b => b);
+            return adjacentPositions.Count(b => b);
         }
 
         int[][] Minesweeper(bool[][] matrix)
         {
-            return new int[1][];
+            List<List<int>> minesweeper = new List<List<int>>();
+            for(int row = 0; row < matrix.Length; row++)
+            {
+                minesweeper.Add(new List<int>());
+                for(int col = 0; col < matrix[row].Length; col++)
+                {
+                    minesweeper[row].Add(MineCounter(matrix, (row, col)));
+                }
+            }
+            return minesweeper.Select(l => l.ToArray()).ToArray();
         }
 
         static void Main(string[] args)
