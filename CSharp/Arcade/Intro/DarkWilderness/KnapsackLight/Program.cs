53d2a15308e7accf3c4dac68ba90df73f4d4784e
aleanvalle <alexv4lle@gmail.com>
2024-02-29 12:58:35 -0300

    agregado KnapsackLight

---
+++ CSharp/Arcade/Intro/DarkWilderness/KnapsackLight/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace KnapsackLight
+{
+    public class Program
+    {
+        int KnapsackLight(int value1, int weight1, int value2, int weight2, int maxW) 
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
