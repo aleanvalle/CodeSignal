76a18554578fc58553437621c8ea0c8860fc69bc
aleanvalle <alexv4lle@gmail.com>
2024-03-01 19:02:28 -0300

    agregado LongestDigitsPrefix

---
+++ CSharp/Arcade/Intro/DarkWilderness/LongestDigitsPrefix/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LongestDigitsPrefix
+{
+    public class Program
+    {
+        public string LongestDigitsPrefix(string inputString)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
