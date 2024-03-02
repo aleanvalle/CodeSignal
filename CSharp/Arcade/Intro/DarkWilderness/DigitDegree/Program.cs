f072eb214f2a530d54f12a623de3a8e70da916a8
aleanvalle <alexv4lle@gmail.com>
2024-03-02 09:11:45 -0300

    agregado DigitDegree

---
+++ CSharp/Arcade/Intro/DarkWilderness/DigitDegree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitDegree
+{
+    public class Program
+    {
+        public int DigitDegree(int n)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
