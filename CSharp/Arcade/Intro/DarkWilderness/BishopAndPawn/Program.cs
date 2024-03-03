5abfc908193c4e3d6d44e1572f0167ba68c13127
aleanvalle <alexv4lle@gmail.com>
2024-03-03 20:41:59 -0300

    agregado BishopAndPawn

---
+++ CSharp/Arcade/Intro/DarkWilderness/BishopAndPawn/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BishopAndPawn
+{
+    public class Program
+    {
+        public bool BishopAndPawn(string bishop, string pawn)
+        {
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
