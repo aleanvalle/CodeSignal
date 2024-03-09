608fe46d208736a4b8abcf4e9193f0785ade276f
aleanvalle <alexv4lle@gmail.com>
2024-03-08 23:13:41 -0300

    agregada posible solucion

--- CSharp/Arcade/Intro/EruptionofLight/BuildPalindrome/Program.cs
+++ CSharp/Arcade/Intro/EruptionofLight/BuildPalindrome/Program.cs
@@ -2,6 +2,27 @@
 {
     public class Program
     {
+        string AddDifferent(string normal, string reversed)
+        {
+            string addDifferent = "";
+            for(int i = 0; i < normal.Length; i++)
+            {
+                for(int j = 0; j < reversed.Length;)
+                {
+                    if (normal[i] == reversed[j])
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        addDifferent.Insert(0, normal[i].ToString());
+                    }
+                }
+            }
+            return normal + addDifferent;
+        }
+
+
         public string BuildPalindrome(string st)
         {
             return "";
@@ -9,7 +30,7 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
