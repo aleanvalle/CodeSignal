1492bce86b3c83444279c3df871a4080d53fc6ae
aleanvalle <alexv4lle@gmail.com>
2024-03-19 11:45:55 -0300

    agregado DigitsProduct

---
+++ CSharp/Arcade/Intro/LandofLogic/DigitsProduct/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitsProduct
+{
+    public class Program
+    {
+        public int DigitsProduct(int product)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
