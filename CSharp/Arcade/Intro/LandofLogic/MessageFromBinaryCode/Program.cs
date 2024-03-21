74086addc5fe940f7875eaa849bf3e416f6f8a53
aleanvalle <alexv4lle@gmail.com>
2024-03-21 18:08:27 -0300

    agregado spiralNumbers, complatado MessageFromBinaryCode y MessageFromeBinaryCode.Test

---
+++ CSharp/Arcade/Intro/LandofLogic/MessageFromBinaryCode/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MessageFromBinaryCode
+{
+    public class Program
+    {
+        char ConvertBinaryStringToChar(string aByte)
+        {
+            int EXPONENT = 7;
+            double sum = 0;
+            for(int i = 0; i < aByte.Length; i++) 
+            {
+                if (aByte[i] == '1')
+                {
+                    sum += Math.Pow(2, EXPONENT);
+                }
+                EXPONENT--;
+            }
+            return (char) sum;
+        }
+
+        public string MessageFromBinaryCode(string code)
+        {
+            string aByte = "";
+            string hiddenMessage = "";
+            for(int i = 0; i < code.Length; i++)
+            {
+                if (aByte.Length == 8)
+                {
+                    hiddenMessage += ConvertBinaryStringToChar(aByte);
+                    aByte = "";
+                }
+                aByte += code[i];
+                if (i + 1 == code.Length)
+                {
+                    hiddenMessage += ConvertBinaryStringToChar(aByte);
+                }
+            }
+            return hiddenMessage;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp/Arcade/Intro/LandofLogic/SpiralNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SpiralNumbers
+{
+    public class Program
+    {
+        public int[][] SpiralNumbers(int n)
+        {
+            return new int[n][];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
